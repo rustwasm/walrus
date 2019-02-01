31365d66f71116ec656c0ee9a09b57e9683008ca
Alex Crichton <alex@alexcrichton.com>
2019-02-01 11:19:05 -0800

    Tweak the round-trip example to optionall emit wasm
    Should help with some debugging of tests and whatnot

--- examples/round-trip.rs
+++ examples/round-trip.rs
@@ -4,5 +4,8 @@ fn main() {
     env_logger::init();
     let a = std::env::args().nth(1).unwrap();
     let m = walrus::module::Module::from_file(&a).unwrap();
-    m.emit_wasm().unwrap();
+    let wasm = m.emit_wasm().unwrap();
+    if let Some(destination) = std::env::args().nth(2) {
+        std::fs::write(destination, wasm).unwrap();
+    }
 }

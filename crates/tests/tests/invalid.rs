fb6a699c3bb64445ab13c214721ab2aa286ba388
Nick Fitzgerald <fitzgen@gmail.com>
2019-08-29 14:40:21 -0700

    Add testing infrastructure for asserting that some Wasm is invalid

---
+++ crates/tests/tests/invalid.rs
@@ -0,0 +1,21 @@
+use std::env;
+use std::path::Path;
+use std::sync::Once;
+
+fn run(wat: &Path) -> Result<(), failure::Error> {
+    static INIT_LOGS: Once = Once::new();
+    INIT_LOGS.call_once(|| {
+        env_logger::init();
+    });
+
+    let wasm = walrus_tests_utils::wat2wasm(wat)?;
+
+    // NB: reading the module will do the validation.
+    if let Ok(_) = walrus::Module::from_buffer(&wasm) {
+        failure::bail!("expected {} to be invalid, but it was valid", wat.display());
+    }
+
+    Ok(())
+}
+
+include!(concat!(env!("OUT_DIR"), "/invalid.rs"));

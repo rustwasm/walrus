e79b0deb0cb87d14df66a91ab77503aa48333d15
Nick Fitzgerald <fitzgen@gmail.com>
2019-09-04 13:35:53 -0700

    Make the `raw` fuzzing target assert deterministic serialization

--- fuzz/fuzz_targets/raw.rs
+++ fuzz/fuzz_targets/raw.rs
@@ -4,5 +4,16 @@
 extern crate libfuzzer_sys;
 
 fuzz_target!(|data: &[u8]| {
-    let _ = walrus::Module::from_buffer(data);
+    let module = match walrus::Module::from_buffer(data) {
+        Ok(m) => m,
+        Err(_) => return,
+    };
+    let serialized = module.emit_wasm();
+    let module =
+        walrus::Module::from_buffer(&serialized).expect("we should only emit valid Wasm data");
+    let reserialized = module.emit_wasm();
+    assert_eq!(
+        serialized, reserialized,
+        "emitting wasm should be deterministic"
+    );
 });

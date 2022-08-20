4e43f90a2dfcde4e105481e6d62f2f81ddc68106
nokotan <kamenokonokotan@gmail.com>
2022-08-20 15:54:42 +0900

    start using enum CodeAddress

--- src/module/debug/mod.rs
+++ src/module/debug/mod.rs
@@ -1,8 +1,10 @@
 mod dwarf;
 
 use crate::emit::{Emit, EmitContext};
-use crate::{CustomSection, FunctionKind, Module, RawCustomSection};
+use crate::{CustomSection, Function, FunctionKind, InstrLocId, Module, RawCustomSection};
 use gimli::*;
+use id_arena::Id;
+use std::cell::RefCell;
 use std::cmp::Ordering;
 
 use self::dwarf::ConvertContext;
@@ -12,6 +14,115 @@ use self::dwarf::ConvertContext;
 pub struct ModuleDebugData {
     /// DWARF debug data
     pub dwarf: read::Dwarf<Vec<u8>>,
+}
+
+enum CodeAddress {
+    InstrInFunction {
+        instr_id: InstrLocId,
+    },
+    OffsetInFunction {
+        id: Id<Function>,
+        offset: usize,
+    },
+    FunctionEdge {
+        previous_id: Option<Id<Function>>,
+        current_id: Id<Function>,
+    },
+    Unknown,
+}
+
+/// Converts original code address to CodeAddress
+struct CodeAddressConverter {
+    /// Function range based convert table
+    address_convert_table: Vec<(wasmparser::Range, Id<Function>)>,
+    /// Instrument based convert table
+    instrument_address_convert_table: Vec<(usize, InstrLocId)>,
+}
+
+impl CodeAddressConverter {
+    fn from_emit_context(cx: &EmitContext) -> Self {
+        let mut address_convert_table = cx
+            .code_transform
+            .function_ranges
+            .iter()
+            .filter_map(|(func_id, _)| match cx.module.funcs.get(*func_id).kind {
+                FunctionKind::Local(ref func) => {
+                    func.original_range.map(|range| (range, func_id.clone()))
+                }
+                _ => None,
+            })
+            .collect::<Vec<_>>();
+
+        let mut instrument_address_convert_table = cx
+            .code_transform
+            .function_ranges
+            .iter()
+            .filter_map(|(func_id, _)| match cx.module.funcs.get(*func_id).kind {
+                FunctionKind::Local(ref func) => Some(&func.instruction_mapping),
+                _ => None,
+            })
+            .flatten()
+            .map(|x| x.clone())
+            .collect::<Vec<_>>();
+
+        address_convert_table.sort_by_key(|i| i.0.start);
+        instrument_address_convert_table.sort_by_key(|i| i.0);
+
+        Self {
+            address_convert_table,
+            instrument_address_convert_table,
+        }
+    }
+
+    fn find_address(&self, address: usize) -> CodeAddress {
+        match self
+            .instrument_address_convert_table
+            .binary_search_by_key(&address, |i| i.0)
+        {
+            Ok(id) => {
+                return CodeAddress::InstrInFunction {
+                    instr_id: self.instrument_address_convert_table[id].1,
+                }
+            }
+            Err(_) => {}
+        };
+
+        let range_comparor = |range: &(wasmparser::Range, _)| {
+            if range.0.end <= address {
+                Ordering::Less
+            } else if address < range.0.start {
+                Ordering::Greater
+            } else {
+                Ordering::Equal
+            }
+        };
+
+        match self.address_convert_table.binary_search_by(range_comparor) {
+            Ok(i) => {
+                let entry = &self.address_convert_table[i];
+                let code_offset_from_function_start = address - entry.0.start;
+
+                if code_offset_from_function_start == 0 {
+                    let previous_id = if i > 0 {
+                        Some(self.address_convert_table[i - 1].1)
+                    } else {
+                        None
+                    };
+
+                    CodeAddress::FunctionEdge {
+                        previous_id,
+                        current_id: entry.1,
+                    }
+                } else {
+                    CodeAddress::OffsetInFunction {
+                        id: entry.1,
+                        offset: code_offset_from_function_start,
+                    }
+                }
+            }
+            Err(_) => CodeAddress::Unknown,
+        }
+    }
 }
 
 impl Module {
@@ -39,84 +150,61 @@ impl Module {
 
 impl Emit for ModuleDebugData {
     fn emit(&self, cx: &mut EmitContext) {
-        let mut address_convert_table = cx
-            .code_transform
-            .function_ranges
-            .iter()
-            .filter_map(
-                |(func_id, func_range)| match cx.module.funcs.get(*func_id).kind {
-                    FunctionKind::Local(ref func) => {
-                        func.original_range.map(|range| (range, func_range))
-                    }
-                    _ => None,
-                },
-            )
-            .map(|(original_range, new_range)| {
-                (
-                    original_range,
-                    (new_range.start - cx.code_transform.code_section_start - original_range.start)
-                        as isize,
-                )
-            })
-            .collect::<Vec<_>>();
-
-        let mut instrument_address_convert_table = cx
-            .code_transform
-            .function_ranges
-            .iter()
-            .filter_map(|(func_id, _)| match cx.module.funcs.get(*func_id).kind {
-                FunctionKind::Local(ref func) => Some(&func.instruction_mapping),
-                _ => None,
-            })
-            .flatten()
-            .map(|x| x.clone())
-            .collect::<Vec<_>>();
-
-        address_convert_table.sort_by_key(|i| i.0.start);
-        instrument_address_convert_table.sort_by_key(|i| i.0);
+        let address_converter = CodeAddressConverter::from_emit_context(cx);
 
         let code_transform = &cx.code_transform;
-        let convert_range_address = |address: u64| -> u64 {
-            let address = address as usize;
-            let comparor = |range: &(wasmparser::Range, isize)| {
-                if range.0.end < address {
-                    Ordering::Less
-                } else if address < range.0.start {
-                    Ordering::Greater
-                } else {
-                    Ordering::Equal
-                }
-            };
-            match address_convert_table.binary_search_by(comparor) {
-                Ok(i) => (address as i64 + address_convert_table[i].1 as i64) as u64,
-                Err(_) => 0,
-            }
-        };
-
         let instruction_map = &code_transform.instruction_map;
-        let convert_instrument_address = |address: u64| -> Option<u64> {
-            let address = address as usize;
-
-            let instr_id =
-                match instrument_address_convert_table.binary_search_by_key(&address, |i| i.0) {
-                    Ok(id) => instrument_address_convert_table[id].1,
-                    Err(_) => {
-                        return None;
-                    }
-                };
-
-            match instruction_map.binary_search_by_key(&instr_id, |i| i.0) {
-                Ok(id) => {
-                    Some((instruction_map[id].1 - cx.code_transform.code_section_start) as u64)
-                }
-                Err(_) => None,
-            }
-        };
+        let refer_previous_function_at_function_edge = RefCell::from(false);
 
         let convert_address = |address| -> Option<write::Address> {
-            convert_instrument_address(address)
-                .or(Some(convert_range_address(address)))
-                .map(|x| write::Address::Constant(x))
+            let address = address as usize;
+            let address = match address_converter.find_address(address) {
+                CodeAddress::InstrInFunction { instr_id } => {
+                    match instruction_map.binary_search_by_key(&instr_id, |i| i.0) {
+                        Ok(id) => Some(
+                            (instruction_map[id].1 - cx.code_transform.code_section_start) as u64,
+                        ),
+                        Err(_) => None,
+                    }
+                }
+                CodeAddress::OffsetInFunction { id, offset } => {
+                    match code_transform
+                        .function_ranges
+                        .binary_search_by_key(&id, |i| i.0)
+                    {
+                        Ok(id) => {
+                            Some((code_transform.function_ranges[id].1.start + offset) as u64)
+                        }
+                        Err(_) => None,
+                    }
+                }
+                CodeAddress::FunctionEdge {
+                    previous_id,
+                    current_id,
+                } => {
+                    let id = if *refer_previous_function_at_function_edge.borrow() {
+                        if !previous_id.is_some() {
+                            return None;
+                        } else {
+                            previous_id.unwrap()
+                        }
+                    } else {
+                        current_id
+                    };
+                    match code_transform
+                        .function_ranges
+                        .binary_search_by_key(&id, |i| i.0)
+                    {
+                        Ok(id) => Some((code_transform.function_ranges[id].1.start) as u64),
+                        Err(_) => None,
+                    }
+                }
+                CodeAddress::Unknown => None,
+            };
+
+            address
+                .or(Some(0))
+                .map(|address| write::Address::Constant(address))
         };
 
         let from_dwarf = cx
@@ -133,6 +221,10 @@ impl Emit for ModuleDebugData {
 
         while let Some(from_unit) = from_units.next().expect("") {
             unit_entries.push(from_unit);
+        }
+
+        {
+            refer_previous_function_at_function_edge.replace(true);
         }
 
         let mut convert_context = ConvertContext::new(&from_dwarf, &convert_address);
